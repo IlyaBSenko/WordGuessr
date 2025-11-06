294838408c9677997f9fa24f8580b0070765c676
Ilya Balatsenko <balatsir@dukes.jmu.edu>
2025-11-06 09:55:18 -0500

    Code spacing

--- main.py
+++ main.py
@@ -20,6 +20,9 @@ def next_word():
     result_label.config(text="")
     entry.focus_set()
     
+def change_theme():
+    window.config()
+    
     
 
 window = Tk()
@@ -38,28 +41,36 @@ sorted_string = "".join(sorted_word)
 title_label = Label(window, text="Word Guessr", font=("Segoe Script Bold", 24, "bold"), bg="#FFE5B4", fg="#4B3832")  # peach & brown palette
 title_label.pack(pady=20)
 
+
 guess_word_label = Label(window, text="Guess the word:", font=("Palatino Linotype Bold Italic", 16), bg="#FFE5B4", fg="#4B3832")
 guess_word_label.pack(pady=21)
 
+
 rand_word = Label(window, text=sorted_string, font=("Palatino Linotype Bold Italic", 12), bg="#FFE5B4", fg="#4B3832")
 rand_word.pack()
+
 
 entry = Entry(window, font=("Palatino Linotype Bold Italic", 14), fg="#4B3832", bg="#FFF8E1")  # brown text on soft cream background
 entry.place(relx=0.5, rely=0.7, anchor="center") 
 entry.focus_set() # clutch lets you start typing immediately without needing to click the box first
 
+
 result_label = Label(window, text="", bg="#FFE5B4", fg="#4B3832", font=("Palatino Linotype Bold Italic", 14))
 result_label.pack(pady=8)
+
 
 submit_button = Button(window, text="Enter", bg="#FFDAB9", fg="#4B3832", command=submit)  # peach button
 submit_button.place(relx=0.3, rely=0.9, anchor="center")
 
+
 next_button = Button(window, text="Next Word", bg="#FFDAB9", fg="#4B3832", command=next_word)  # matching peach button
 next_button.place(relx=0.7, rely=0.9, anchor="center")
 
-# change_color = Button(window, text="Change Theme", bg="#FFDA89", fg="#4B3832", command=change_color)
-# change_color.place(relx=0.5, rely=0.9, anchor="center")
+
+change_color = Button(window, text="Change Theme", bg="#FFDA89", fg="#4B3832", command=change_theme)
+change_color.place(relx=0.5, rely=0.9, anchor="center")
+
+
 
 window.bind("<Return>", lambda e: submit()) # connects keyboard enter button to submit button, clutch
-
 window.mainloop()
